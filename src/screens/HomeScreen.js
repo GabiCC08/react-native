daef7530f70a97189856ca36cea2860d8fe237fe
DESKTOP-VFUJHKU\gaby2 <gaby.2ax.88@gmail.com>
2020-12-19 22:38:40 -0500

    Botones y Navegacion

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hola :D</Text>;
+const HomeScreen = (props) => {
+  return (
+      <View>
+        <Text style={styles.text}>Hi :D</Text>
+        <Button
+            onPress={() => props.navigation.navigate('Components')}
+            title="Go to Components Demo"
+        />
+        <Button
+            onPress={() => props.navigation.navigate('List')}
+            title="Go to List Demo"
+        />
+      </View>
+
+  );
 };
 
 const styles = StyleSheet.create({
